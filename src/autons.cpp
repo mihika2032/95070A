899f8efce819ecf555a88acde1797ae668a80e11
mihika2032 <mihika2032@gmail.com>
2026-07-30 15:13:52 -0700

    auton path starting

--- src/autons.cpp
+++ src/autons.cpp
@@ -14,20 +14,16 @@ using namespace vex;
 void rightAuton() {
   DrivePidParams setting = {};  //testing that there are no errors when building-- still need to test downloading
   setting.maxSpeed = 60.0;
-  drivePID(12);
-  turnPID(-88,0.30,0,0); 
-  
-  Controller.Screen.clearLine(3);
-  Controller.Screen.setCursor(3, 1);
-  Controller.Screen.print("start auton");
 
-  drive Chassis(3.25, 4.0/3, 24, 0, 0, leftDrive, rightDrive, PORT1);
+  drive Chassis(3.25, 4.0/3, 72, 12, 0, leftDrive, rightDrive, PORT1);
+  drivePID(-6);
+  toggle.spin(forward, 100, percent);
+  c
 
-  Chassis.driveDistance(24, 1, 1, 0, 0.1);
+
 }
 
 void leftAuton() {
-  turnPID(-90,1,1,1);
 
   //Use functions to create an auton for the left side of the field
 
